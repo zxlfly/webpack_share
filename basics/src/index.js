02659f1768ea77c1ca952066aa974dce11b5f370
zxlfly <zxlfly@foxmail.com>
2020-12-24 00:07:12 +0800

    update basics

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,1 +1,2 @@
+import css from './index.css'
 console.log('hello webpack')
--- basics/webpack.config.js
+++ basics/webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin =require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     // 必填 webpack执⾏构建⼊⼝
     //既支持相对路径也支持绝对路径
     entry: "./src/index.js",
+    // 多页面形式
+    // entry:{
+    //     main:"./src/index.js",
+    //     about:'./src/about.js'
+    // }
     output: {
         // 将所有依赖的模块合并输出到main.js
         //只支持绝对路径
@@ -11,5 +18,38 @@ module.exports = {
         path: path.resolve(__dirname, "./dist")
     },
     // 开发模式（不会压缩代码）
-    mode:'development'
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // loader执行顺序从右到左
+                use: [ 'style-loader','css-loader' ]
+            }
+        ]
+    },
+    plugins:[
+        // 生成html
+        new HtmlWebpackPlugin({
+            template:"./src/index.html",
+            filename:"index.html",
+            minify
+        }),
+        // 打包清空dist冗余文件
+        new CleanWebpackPlugin()
+    ]
+    // 多页面就写多个
+    // chunks写入当前页面需要关联的文件，对应入口
+    // plugins:[
+    //     new HtmlWebpackPlugin({
+    //         template:"./src/index.html",
+    //         filename:"index.html",
+    //         chunks:['main','list']
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         template:"./src/about.html",
+    //         filename:"about.html",
+    //         chunks:['main','about']
+    //     })
+    // ]
 };
